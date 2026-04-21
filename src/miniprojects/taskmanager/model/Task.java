f3c60fb6a9ece4f47d29c8966f862a4c8dde9537
GustavoOMendonca <gustavodeoliveira3443@gmail.com>
2026-04-21 17:12:49 -0300

    feat: adiciona classe Task com métodos para gerenciamento de tarefas

---
+++ src/miniprojects/taskmanager/model/Task.java
@@ -0,0 +1,28 @@
+package miniprojects.taskmanager.model;
+
+public class Task {
+    private String description;
+    private boolean completed;
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void markAsCompleted() {
+        this.completed = true;
+    }
+
+    @Override
+    public String toString() {
+        return (completed ? "[x] " : "[ ] ") + description;
+    }
+}
